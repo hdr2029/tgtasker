15b21a2bc365b2ddd923adeb6227c65885787871
nikich <kyivelit@gmail.com>
2025-10-05 18:40:23 +0300

    Update twenty-second channel to краще перезняти!

--- src/context/AppStateContext.jsx
+++ src/context/AppStateContext.jsx
@@ -25,7 +25,7 @@ const initialChannels = [
   { id: "dostoverkin", name: "Труха ⚡️Crypto", audience: "73K", avatar: "https://static4.tgstat.ru/channels/_0/38/38ea9470e2b83bc4f26f7c31e930b453.jpg", rewardTon: 0.19, link: "https://t.me/cryptotruexa" },
   { id: "cryptonomy", name: "Українці на Кіпрі", audience: "19K", avatar: "https://static8.tgstat.ru/channels/_0/93/939e219b8c596ebe015e6c33605146b4.jpg", rewardTon: 0.27, link: "https://t.me/uacyprus" },
   { id: "lazyinvestor", name: "🇺🇦 Ukraine Digital", audience: "9K", avatar: "https://static5.tgstat.ru/channels/_0/d5/d5899c9d8b19dc533db1e2d695af8c9b.jpg", rewardTon: 0.34, link: "https://t.me/ukraine_digital" },
-  { id: "nishqua", name: "nishqua", audience: "172.4K", avatar: "https://dummyimage.com/96x96/0f172a/ffffff&text=NQ", rewardTon: 0.36, link: "https://t.me/nishqua" },
+  { id: "nishqua", name: "Краще перезняти!", audience: "24K", avatar: "https://static10.tgstat.ru/channels/_0/4c/4c3b22df2fc1d7efbc07d5e7a78f354a.jpg", rewardTon: 0.36, link: "https://t.me/bshootua" },
   { id: "masterpoll", name: "Sponsored by MasterPoll", audience: "774.3K", avatar: "https://dummyimage.com/96x96/f43f5e/ffffff&text=MP", rewardTon: 0.45, link: "https://t.me/masterpoll" },
 ];
 
